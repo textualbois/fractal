c731587d506f7832a2ec3a0e70a70f58773f709c
textualbois <wivna360@hotmail.com>
2024-04-05 00:44:01 +0200

    added input checks

--- check_input.c
+++ check_input.c
@@ -1,18 +1,74 @@
 #include "./fractol.h"
 
+static int check_mandelbrot(int argc, char **argv)
+{
+	if (argc >= 3)
+	{
+		if (ft_isint(argv[2]) == 0)
+		{
+			printf("second parameter should be int\n");
+			return (0);
+		}
+		else if (argc >= 4)
+		{
+			printf("Wrong argument count\n0 or 1 extra allowed\n");
+			return (0);
+		}
+	}
+	return (1);
+}
 
+static int check_julia(int argc, char **argv)
+{
+	if (argc >= 3)
+	{
+		if (ft_isint(argv[2]) == 0)
+		{
+			printf("second parameter should be int\n");
+			return (0);
+		}
+		else if (argc == 4 || argc >= 6)
+		{
+			printf("Wrong argument count\n0, 1 or 2 extra allowed\n");
+			return (0);
+		}
+		else if (!ft_isdouble(argv[3]) || !ft_isdouble(argv[4]))
+		{
+			printf("Arguments 3 and 4 have to be of type double or int\n");
+			return (0);
+		}
+	}
+	return (1);
+}
 
+static int check_debug(int argc, char **argv)
+{
+	if (argc >= 2)
+	{
+		return (0);
+	}
+	return (1);
+}
 
 int check_input(int argc, char **argv)
 {
-	if (argc == 1)
-		return (0);
-	if (!ft_strncmp(argv[1], "julia\0", 6))
-		return (check_julia(argc, argv));
-	else if (!ft_strncmp(argv[1], "mandelbrot\0", 10))
-		return (check_mandelbrot(argc, argv));
-	else if (!ft_strncmp(argv[1], "debug\0", 5))
-		return (check_debug(argc, argv));
+	if (argc > 1)
+	{
+		if (!ft_strncmp(argv[1], "julia\0", 6))
+			return (check_julia(argc, argv));
+		else if (!ft_strncmp(argv[1], "mandelbrot\0", 10))
+			return (check_mandelbrot(argc, argv));
+		else if (!ft_strncmp(argv[1], "debug\0", 5))
+			return (check_debug(argc, argv));
+		else
+		{
+			printf("Specify fractal in first parameter\n");
+			return (0);
+		}
+	}
 	else
+	{
+		printf("At least 1 parameter required\n");
 		return (0);
+	}
 }
