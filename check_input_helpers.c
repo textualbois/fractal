cf4b06672e1645d67d78d1b7fe637ff84d9e7579
textualbois <wivna360@hotmail.com>
2024-04-04 23:18:40 +0200

    numeric value checker functions

--- check_input_helpers.c
+++ check_input_helpers.c
@@ -1,8 +1,45 @@
 #include "./fractol.h"
 
-int	ft_isdigit(int c)
+int ft_isdouble(char *str)
 {
-	if (c >= '0' && c <= '9')
+	int	i;
+	int	dot_count;
+	int	sign_count;
+
+	i = 0;
+	dot_count = 0;
+	sign_count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '+' || str[i] == '-')
+			sign_count++;
+		else if (str[i] == '.')
+			dot_count++;
+		else if (ft_isdigit(str[i]) == 0)
+			return (0);
+		i++;
+	}
+	if (dot_count < 2 && sign_count < 2 && i > 0)
+		return (1);
+	return (0);
+}
+
+int ft_isint(char *str)
+{
+	int	i;
+	int	sign_count;
+
+	i = 0;
+	sign_count = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '+' || str[i] == '-')
+			sign_count++;
+		else if (ft_isdigit(str[i]) == 0)
+			return (0);
+		i++;
+	}
+	if (sign_count < 2 && i > 0)
 		return (1);
 	return (0);
 }
