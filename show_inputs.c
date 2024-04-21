90770223dc0b2952a1d84ef49b7931e827682309
textualbois <wivna360@hotmail.com>
2024-04-21 20:44:35 +0200

    added list functions to default build

--- show_inputs.c
+++ show_inputs.c
@@ -6,7 +6,7 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 20:21:48 by isemin            #+#    #+#             */
-/*   Updated: 2024/04/21 20:33:51 by isemin           ###   ########.fr       */
+/*   Updated: 2024/04/21 20:41:35 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	show_inputs(void)
 {
-	ft_printf("Allowed fractals:\n\nmandelbrot p1 - precision\n\n\
-	julia p1 - precision p2-3 - modifiers\n\n\
-	burning ship p1 - precision\n\n\n\
+	ft_printf("Allowed fractals:\n\n	mandelbrot: p1 - precision\n\n\
+	julia: p1 - precision; p2-3 - modifiers\n\n\
+	burning ship: p1 - precision\n\n\n\
 	controls:\n\
 	r - boost red color\n\
 	g - boost green color\n\
@@ -24,5 +24,7 @@ void	show_inputs(void)
 	a - boost intensity of all colors\n\
 	space bar - add static effect\n\
 	p - add acid effect\n\
-	o - change acid effect parameters\n");
+	o - change acid effect parameters\n\
+	scroll wheel - zoom\n\
+	arrow keys - movement");
 }
