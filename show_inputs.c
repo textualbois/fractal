18e6f4d8e8bbc33cbca6b6585681db0630989160
textualbois <wivna360@hotmail.com>
2024-04-24 18:58:11 +0200

    changed program description

--- show_inputs.c
+++ show_inputs.c
@@ -6,7 +6,7 @@
 /*   By: isemin <isemin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 20:21:48 by isemin            #+#    #+#             */
-/*   Updated: 2024/04/21 20:53:07 by isemin           ###   ########.fr       */
+/*   Updated: 2024/04/24 18:57:36 by isemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 void	show_inputs(void)
 {
-	ft_printf("Allowed fractals:\n\n	mandelbrot: p1 - precision\n\n\
-	julia: p1 - precision; p2-3 - modifiers\n\n\
-	burning ship: p1 - precision\n\n\n\
+	ft_printf("Allowed fractals:\n\n
+	mandelbrot: <max_iterations>\n\n\
+	julia: <max_iterations> <modifier_1 modifier_2>\n\n\
+	burning ship: <max_iterations>\n\n\n\
 	controls:\n\
 	r - boost red color\n\
 	g - boost green color\n\
 	b - boost blue color\n\
-	a - boost intensity of all colors\n\
+	a - boost opacity\n\
 	space bar - add static effect\n\
 	p - add acid effect\n\
 	o - change acid effect parameters\n\
